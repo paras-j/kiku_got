70464eae8f0321565e5c34e1b44b8f07a363ed1a
paras-j <59125176+paras-j@users.noreply.github.com>
2022-03-28 16:51:40 -0400

    Update got.py

--- got.py
+++ got.py
@@ -1,27 +1,27 @@
 import streamlit as st
 
-from haystack.utils import clean_wiki_text, convert_files_to_dicts, fetch_archive_from_http, print_answers
+#from haystack.utils import clean_wiki_text, convert_files_to_dicts, fetch_archive_from_http, print_answers
 from haystack.nodes import FARMReader, TransformersReader
 from haystack.document_stores import FAISSDocumentStore
 from haystack.nodes import DensePassageRetriever
 from haystack.nodes import FARMReader, TransformersReader
 from haystack.pipelines import ExtractiveQAPipeline
 
-@st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def get_retriever():
-    document_store = FAISSDocumentStore.load("haystack_got_faiss_0322")
-    st.write("I got document_store in retreiver")
-    retriever = DensePassageRetriever(document_store=document_store, query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki", passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",)
-    st.write("I got retriever")
-    return retriever
+# @st.cache(allow_output_mutation=True, suppress_st_warning=True)
+# def get_retriever():
+document_store = FAISSDocumentStore.load("haystack_got_faiss_0322")
+st.write("I got document_store in retreiver")
+retriever = DensePassageRetriever(document_store=document_store, query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki", passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",)
+st.write("I got retriever")
+#     return retriever
 
-@st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def get_reader():
-    st.write("I am inside reader")
-    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=False)
-    #reader = TransformersReader(model_name_or_path="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased", use_gpu=-1)
-    st.write("I got reader")
-    return reader
+# @st.cache(allow_output_mutation=True, suppress_st_warning=True)
+# def get_reader():
+st.write("I am inside reader")
+#reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=False)
+reader = TransformersReader(model_name_or_path="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased", use_gpu=-1)
+st.write("I got reader")
+#     return reader
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 st.title("Question Answering Webapp")
@@ -37,6 +37,7 @@ if text:
 st.write("Response:")
 with st.spinner('Searching for answers....'):
     st.write("I'm in spinner")
-    prediction = pipe.run(query=text, params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 1}})
+    prediction = pipe.run(query=text, params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 2}})
     st.write('answer: {}'.format(prediction['answers'][0].answer))
+    st.write('answer: {}'.format(prediction['answers'][1].answer))
 st.write("")
